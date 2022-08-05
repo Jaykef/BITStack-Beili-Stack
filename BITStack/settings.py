fb6578b697a2d15d69a042645942c47a61d086ba
Jaward Sesay <jkef96@icloud.com>
2022-08-05 12:43:35 +0800

    Update settings.py

--- BITStack/settings.py
+++ BITStack/settings.py
@@ -144,11 +144,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#ACCOUNT_EMAIL_VERIFICATION = "none"
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bitstackapps@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bitstack2022'
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_EMAIL_VERIFICATION = "none"
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
+#EMAIL_PORT = 587
